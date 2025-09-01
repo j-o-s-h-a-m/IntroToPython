34bd97b5c349d76c08880bd806bde28fae1f2279
j-o-s-h-a-m <22SJoshi.acc@lwetb.ie>
2025-09-01 21:34:00 +0100

    Update IntroToPython2.py

--- IntroToPython2.py
+++ IntroToPython2.py
@@ -51,5 +51,24 @@ no_diners = int(no_dineers)
 each_p = total_bill/no_diners
 print('Each person should pay',each_p)
 
+no_days = input('enter a random number of days : ')
+no_days = int(no_days)
+hours = no_days*24
+minutes = no_days*24*60
+seconds = no_days*24*60*60
+
+if no_days > 1 :
+ print('there are',hours,'hours',minutes,'minutes and',seconds,'seconds in',no_days,'days')
+else :
+ print('there are',hours,'hours',minutes,'minutes and',seconds,'seconds in a day')
+
+pounds_lbs = input('Enter Wweight in pounds : ')
+pounds_lbs = int(pounds_lbs)
+
+kgs = pounds_lbs*2.204
+
+print('there are',kgs,'kilograms in',pounds_lbs,'pounds')
+
           
+
 
