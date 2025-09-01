84255f28bb847f744a0637c52be23225c256c29f
j-o-s-h-a-m <22SJoshi.acc@lwetb.ie>
2025-09-01 12:58:54 +0100

    Add files via upload

---
+++ IntroToPython2.py
@@ -0,0 +1,55 @@
+#Athor = Saksham Joshi
+#Date = 1st of Semptember 2025
+#description = Program to request user name as input and return the same with a greeting 
+
+
+user_fname = input('First Name : ')
+user_Surname = input('Surname Name : ')
+print('Hello,' , user_fname, user_Surname)
+
+x = input('Enter Your First Number :  ')
+x = int(x)
+y = input('Enter Your Second name  :  ')
+y = int(y)
+total = x + y
+print(total)
+ 
+First_number = input(' first Number : ')
+First_number = int(First_number)
+
+Second_number = input('Second Number : ')
+Second_number = int(Second_number)
+third_number = input('Third number :  ')
+third_number = int(third_number)
+
+total2 = First_number + Second_number + third_number
+total3 = total + total2
+
+print(total3)
+
+total_slices = input('Total Number of pizza slices : ')
+total_slices = int(total_slices)
+
+slices_eaten = input('Number Of slices You ate : ')
+slices_eaten = int(slices_eaten)
+total_slices = total_slices - slices_eaten
+print(total_slices)
+
+name = input('enter your name : ')
+age = input('enter your age : ')
+age = int(age)
+
+age_next_year = age + 1
+
+print('hello,',name,', next year you will be',age_next_year)
+
+total_bill = input('Enter total Bill : ')
+total_bill = int(total_bill)
+
+no_diners = input('Number of diners : ')
+no_diners = int(no_dineers)
+each_p = total_bill/no_diners
+print('Each person should pay',each_p)
+
+          
+
