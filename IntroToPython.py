de21709ad2f5e56ce411f84b0bc1be03b5504c89
j-o-s-h-a-m <22SJoshi.acc@lwetb.ie>
2025-09-01 12:27:04 +0100

    Add files via upload

---
+++ IntroToPython.py
@@ -0,0 +1,26 @@
+#Part 1
+name ='bob'
+age = 25
+afe = 'Twentyfive'
+print(name , age)
+#Part 2 
+total = 25 % 11
+print(total)
+#Part 3
+remainder = total % age 
+print(remainder)
+#End
+
+user_address = '''Athlone'''
+print(user_address)
+
+user_house_number = 23
+print(type(user_house_number))
+
+print('Hello John', name , age , afe , total , remainder , user_address , user_house_number)
+
+user_age = input('''Enter your age''')
+print(user_age, type(user_age))
+
+user_age = int(user_age)
+print(user_age , type(user_age))
