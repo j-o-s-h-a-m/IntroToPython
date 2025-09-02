c1feabf7e9205fab2699f85927a75dff196082c9
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-02 14:39:49 +0100

    Add files via upload

---
+++ Book Programme 1.py
@@ -0,0 +1,4 @@
+r = input('Enter radius : ')
+r = int(r)
+a = (3.14159)*r**2
+print(a)
---
+++ Book Programme 2.py
@@ -0,0 +1,15 @@
+subject_1 =input('Enter Your 1 subject : ')
+subject_1 =int(subject_1)
+subject_2 =input('Enter Your 2 subject : ')
+subject_2 =int(subject_2)
+subject_3 =input('Enter Your 3 subject : ')
+subject_3 = int(subject_3)
+subject_4 =input('Enter Your 4 subject : ')
+subject_4 = int (subject_4)
+subject_5 =input('Enter Your 5 subject : ')
+subject_5 = int(subject_5)
+
+average = (subject_1 + subject_2 + subject_3 + subject_4 + subject_5)/5
+
+print(average)
+
