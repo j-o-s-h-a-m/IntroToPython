6bbeb6768b64fda29f028d70f3fd22d3e5a964be
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-05 08:43:14 +0000

    yes

---
+++ elif 1.py
@@ -0,0 +1,11 @@
+a = input('enter a value for a = ')
+a = int(a)
+b = input('enter a value for b (b is not equal to a) = ')
+b = int(b)
+
+if (a>b) :
+    print(a)
+    print(b)
+else :
+    print(b)
+    print(a)
