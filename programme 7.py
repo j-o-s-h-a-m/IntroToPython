957c0f114184c5e4a1d2f15a5eff0aa46965c19c
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-06 18:00:55 +0100

    Update programme 7.py

--- programme 7.py
+++ programme 7.py
@@ -1,3 +1,6 @@
+#Athor = Saksham Joshi
+#Date = 1st of Semptember 2025
+#description = Program to request user name as input and return the same with a greeting 
 no_days = input('enter a random number of days : ')
 no_days = int(no_days)
 hours = no_days*24
@@ -8,3 +11,4 @@ if no_days > 1 :
  print('there are',hours,'hours',minutes,'minutes and',seconds,'seconds in',no_days,'days')
 else :
  print('there are',hours,'hours',minutes,'minutes and',seconds,'seconds in a day')
+
