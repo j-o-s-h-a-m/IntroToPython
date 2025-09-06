40dafdbaeed56077da998d3bc622e8477d617e0e
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-06 17:59:21 +0100

    Update Book Programme 6 .py

--- Book Programme 6 .py
+++ Book Programme 6 .py
@@ -1,4 +1,8 @@
+#Athor = Saksham Joshi
+#Date = 1st of Semptember 2025
+#description = Program to request user name as input and return the same with a greeting 
 r = input('Enter radius : ')
 r = int(r)
 a = (3.14159)*r**2
+
 print(a)
