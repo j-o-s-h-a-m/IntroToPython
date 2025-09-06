811cdf3829e3b302fb609949f0f3997b88e84096
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-06 18:00:26 +0100

    Update programme 4.py

--- programme 4.py
+++ programme 4.py
@@ -1,7 +1,11 @@
+#Athor = Saksham Joshi
+#Date = 1st of Semptember 2025
+#description = Program to request user name as input and return the same with a greeting 
 total_slices = input('Total Number of pizza slices : ')
 total_slices = int(total_slices)
 
 slices_eaten = input('Number Of slices You ate : ')
 slices_eaten = int(slices_eaten)
 total_slices = total_slices - slices_eaten
+
 print(total_slices)
