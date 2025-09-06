97fc021b4d6ae0091187fed8e3ed8b94c4a77816
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-06 18:16:44 +0100

    Update elif 2.py

---
+++ elif 2.py
@@ -0,0 +1,14 @@
+#Athor = Saksham Joshi
+#Date = 1st of Semptember 2025
+#description = working with if and else commands
+is_raining = input('is it raining outside : ')
+user_windy = input('is it windy outside : ')
+
+if is_raining == ('yes' or 'Yes') and user_windy == ('yes' or 'yes') :
+  print('carry an umbrella')
+elif is_raining == ('yes' or 'Yes') and user_windy == ('yes' or 'Yes') :
+  print('too windy to carry an umbrella')
+
+else :
+  print('have a good day')
+  
