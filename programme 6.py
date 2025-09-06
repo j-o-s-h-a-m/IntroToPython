15cb4cffc8b88c729c2f1dd0128e46b487db76bc
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-06 18:00:45 +0100

    Update programme 6.py

--- programme 6.py
+++ programme 6.py
@@ -1,3 +1,6 @@
+#Athor = Saksham Joshi
+#Date = 1st of Semptember 2025
+#description = Program to request user name as input and return the same with a greeting 
 total_bill = input('Enter total Bill : ')
 total_bill = int(total_bill)
 
@@ -5,3 +8,4 @@ no_diners = input('Number of diners : ')
 no_diners = int(no_diners)
 each_p = total_bill/no_diners
 print('Each person should pay',each_p)
+
