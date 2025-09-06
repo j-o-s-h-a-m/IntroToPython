e8c7ec8ce45e117dae71d26c4d2f384b39658731
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-06 18:26:41 +0100

    Update elif 3 .py

--- elif 3 .py
+++ elif 3 .py
@@ -1,1 +1,11 @@
+#Athor = Saksham Joshi
+#Date = 1st of Semptember 2025
+#description = working with if and else commands
 
+number = input('Enter a number between 10-20 : ')
+number = int(number)
+
+if number <= 20 and number >= 10 :
+  print('thank you')
+else :
+  print('incorrect value')
