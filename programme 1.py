fee4492b34c77145b4fcc7d7ce1884f681832bf8
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-06 17:59:57 +0100

    Update programme 1.py

--- programme 1.py
+++ programme 1.py
@@ -1,3 +1,7 @@
+#Athor = Saksham Joshi
+#Date = 1st of Semptember 2025
+#description = Program to request user name as input and return the same with a greeting 
 user_fname = input('First Name : ')
 user_Surname = input('Surname Name : ')
+
 print('Hello,' , user_fname, user_Surname)
