5680c7aac6687203a27202702c9a32541a4486f6
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-06 18:00:37 +0100

    Update programme 5.py

--- programme 5.py
+++ programme 5.py
@@ -1,7 +1,11 @@
+#Athor = Saksham Joshi
+#Date = 1st of Semptember 2025
+#description = Program to request user name as input and return the same with a greeting 
 name = input('enter your name : ')
 age = input('enter your age : ')
 age = int(age)
 
 age_next_year = age + 1
+
 
 print('hello,',name,', next year you will be',age_next_year)
