afa61ca7a5cd053aef59a8d7c20cc5977fc772cf
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-06 18:43:30 +0000

    updated input function

--- elif 5.py
+++ elif 5.py
@@ -1,12 +1,12 @@
 #Athor = Saksham Joshi
 #Date = 4th of Semptember 2025
 #description = working with if and else commands 
-is_raining = input('is it raining outside : ')
-user_windy = input('is it windy outside : ')
+is_raining = input('is it raining outside : ').lower()
+user_windy = input('is it windy outside : ').lower()
 
-if is_raining == ('yes' or 'Yes') and user_windy == ('yes' or 'yes') :
+if is_raining == ('yes') and user_windy == ('no') :
   print('carry an umbrella')
-elif is_raining == ('yes' or 'Yes') and user_windy == ('yes' or 'Yes') :
+elif is_raining == ('yes') and user_windy == ('yes') :
   print('too windy to carry an umbrella')
 
 else :
