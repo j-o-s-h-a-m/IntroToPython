dcce567a2a86fc993173ac7f6419811c16a9e151
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-06 18:00:07 +0100

    Update programme 2.py

--- programme 2.py
+++ programme 2.py
@@ -1,6 +1,10 @@
+#Athor = Saksham Joshi
+#Date = 1st of Semptember 2025
+#description = Program to request user name as input and return the same with a greeting 
 x = input('Enter Your First Number :  ')
 x = int(x)
 y = input('Enter Your Second name  :  ')
 y = int(y)
 total = x + y
+
 print(total)
