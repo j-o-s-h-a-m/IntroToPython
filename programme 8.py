9170fea64725a57a0229a27d9958f2c58fba9d6c
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-06 18:01:02 +0100

    Update programme 8.py

--- programme 8.py
+++ programme 8.py
@@ -1,6 +1,10 @@
+#Athor = Saksham Joshi
+#Date = 1st of Semptember 2025
+#description = Program to request user name as input and return the same with a greeting 
 pounds_lbs = input('Enter Wweight in pounds : ')
 pounds_lbs = int(pounds_lbs)
 
 kgs = pounds_lbs*2.204
+
 
 print('there are',kgs,'kilograms in',pounds_lbs,'pounds')
