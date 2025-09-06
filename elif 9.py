a208e858d21ab1b2fb4bf01c9127eb7520c05fbc
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-06 19:08:21 +0100

    Update elif 9.py

--- elif 9.py
+++ elif 9.py
@@ -1,1 +1,12 @@
+units = input('Enter units used : ')
+units = int(units)
 
+if units <= 100  :
+  w5perUnit = units*5
+  print('Total water bill charges is',w5perUnit)
+elif units <= 200 :
+  w10perunit = units*10
+  print('total water bill charges is',w10perunit)
+elif units >= 250 :
+  w20perunit = units*20
+  print('total water bill charges is',w20perunit)
