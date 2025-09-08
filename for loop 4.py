0e2c9cced3d6615f23984baf76e8d920cb2e87dc
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-08 12:46:41 +0100

    Update for loop 4.py

--- for loop 4.py
+++ for loop 4.py
@@ -1,1 +1,11 @@
+#Author = Saksham joshi
+#9/8/2025
+#For loops
 
+user_name = input('Enter Your name : ')
+times = input('Nuber of times you want it to repeat : ')
+time = int(times)
+
+for letter in user_name:
+  for letter in raneg(time):
+    print(letter)
