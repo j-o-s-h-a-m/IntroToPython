652ebff95118686049f55d64adca8361ab59e02e
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-08 12:39:01 +0100

    Update for loop 2.py

--- for loop 2.py
+++ for loop 2.py
@@ -1,1 +1,10 @@
+#Author = Saksham joshi
+#9/8/2025
+#For loops
 
+user_name = input('Enter your name : ')
+times = input('enter the number of times you want your name to print : ')
+time = int(times)
+
+for i in range(time):
+  print(user_name)
