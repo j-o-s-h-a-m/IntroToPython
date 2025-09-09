d869aacc1cb1c185fedf32cfb23ab672e41bd0c6
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-09 14:28:46 +0100

    Update for loop 5.py

--- forloop/for loop 5.py
+++ forloop/for loop 5.py
@@ -1,8 +1,18 @@
 #Author = Saksham Joshi
 #9/8/2025
 #for loops
+total = 0
 
-for total = input('enter your number : ') in range (5):
-  y_or_n = input('dyou wanna add that to the total : ')
-if 
+for number in range(5) :
+    user_number = input('Enter Your Number : ')
+    user_number = int(user_number)
+    add_to_total = input('Add to total? : ').lower()
+    if add_to_total == 'yes' :
+        total = total + user_number
+        print(total)
+        
+    
+    
+
+    
 
