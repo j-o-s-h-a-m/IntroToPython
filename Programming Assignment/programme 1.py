0c40d8abf1c3e02ef93963cdbaebb6b60b468e09
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-11 09:07:09 +0100

    Update programme 1.py

--- Programming Assignment/programme 1.py
+++ Programming Assignment/programme 1.py
@@ -1,3 +1,12 @@
+#Author = Saksham Joshi
+#date = 9/11/2025
+#for loop assignments 
+
+#Version 1 
 for i in range(2,11,2):
   print(i)
-  
+
+#version 2
+for i in range(11):
+    if i%2 == 0 :
+        print(i)
