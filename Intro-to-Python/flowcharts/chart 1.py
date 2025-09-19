153c828f5e9a3b5c06785aa71829f346cad26bb1
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-19 09:43:04 +0100

    Update chart 1.py

--- Intro-to-Python/flowcharts/chart 1.py
+++ Intro-to-Python/flowcharts/chart 1.py
@@ -1,3 +1,6 @@
+#Athor = Saksham Joshi
+#Date = 14 september 2025
+#description = program to print area and length of an rectangle given by an user 
 l = int(input('Enter Length : '))
 w = int(input('Enter Width  : '))
 
