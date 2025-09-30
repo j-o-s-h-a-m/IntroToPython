b48447aaba71d81f32dc7ae1f81b821962f815ac
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-30 13:48:18 +0100

    Update programme 2.py

--- user-input/programme 2.py
+++ user-input/programme 2.py
@@ -2,9 +2,10 @@
 #Date = 1st of Semptember 2025
 #description = Program to request user name as input and return the same with a greeting 
 x = input('Enter Your First Number :  ')
-x = int(x)
-y = input('Enter Your Second name  :  ')
-y = int(y)
+x = float(x)
+y = input('Enter Your Second number  :  ')
+y = float(y)
 total = x + y
 
 print(total)
+
