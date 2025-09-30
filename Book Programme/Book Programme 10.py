2c6d1cfc6e3c25dd3916ae8091527edf9dd7f550
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-09-30 14:38:27 +0100

    Update Book Programme 10.py

--- Book Programme/Book Programme 10.py
+++ Book Programme/Book Programme 10.py
@@ -5,5 +5,5 @@ height = input('Enter Height of a triangle : ')
 height = float(height)
 base = input('Enter Base of a Triangle : ')
 base = float(base)
-area = (height*height)/2
+area = (height*base)/2
 print('Area of the Triangle is',area)
