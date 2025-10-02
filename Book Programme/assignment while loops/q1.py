db6650670b30c1abad7a695b3d77c0723bf5ada9
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-02 09:42:14 +0100

    Update q1.py

--- Book Programme/assignment while loops/q1.py
+++ Book Programme/assignment while loops/q1.py
@@ -1,1 +1,12 @@
+n = -1 #enter your default value
+total = 0 
+count = 0
+while n!='' :
+  n = input('enter your number: ')
+  if n.isdigit():
+    total += int(n)
+    count += 1
+print('average is: ', total/count)
 
+total = total + int(n)
+    
