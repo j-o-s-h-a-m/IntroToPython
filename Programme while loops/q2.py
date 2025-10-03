45ecc289ea80c3baed43574e612d567f101c99e6
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-03 09:45:27 +0100

    Update q2.py

--- Programme while loops/q2.py
+++ Programme while loops/q2.py
@@ -1,1 +1,22 @@
+number = 2
+total = 0
+count = 0
 
+while number != '':
+    number = input('Enter your percentage for each subject: ')
+    if number.isdigit():
+        total += 1
+        count += 1
+        average = total/count
+
+if average >= 90:
+    print('Your Grade is A')
+elif average <=89 and average >=80:
+    print('Your grade is B')
+elif average >=79 and average <= 70:
+    print('your grade is c')
+elif average  >=60 and average <= 69:
+    print('your grade is d')
+else:
+    print('your grade is f')
+    
