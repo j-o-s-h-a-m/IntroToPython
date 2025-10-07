3773748da121bc68205ec3aa21282002028b0a88
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-07 14:09:32 +0100

    Update q6.py

--- Programme while loops/q6.py
+++ Programme while loops/q6.py
@@ -1,1 +1,12 @@
 
+number = int(input('Input a number : '))
+count = 0
+
+while count != 50 :
+    if count < 50 :
+        number = int(input('Input a number : '))
+    else :
+        break
+    
+    count += number
+    print(count)
