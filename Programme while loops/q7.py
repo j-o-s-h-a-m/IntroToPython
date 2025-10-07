806a8cf8bf1f25d70ac283cb7ce7a2f02f1d2742
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-07 14:17:02 +0100

    Update q7.py

--- Programme while loops/q7.py
+++ Programme while loops/q7.py
@@ -1,1 +1,11 @@
+wn = 1
 
+while wn != '' :
+    number = int(input('Enter your number : '))
+    if number >= 10 and number <= 20 :
+        print('thank you')
+        break
+    elif number < 10 :
+        print('Too Low')
+    elif number > 20 :
+        print('Too High')
