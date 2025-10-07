dac3d358f838c99e7bea74b172dd8a12439bfff9
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-07 14:25:32 +0100

    Update q8.py

--- Programme while loops/q8.py
+++ Programme while loops/q8.py
@@ -1,1 +1,15 @@
+wn = 1
+total = 0
 
+while wn != '' :
+    number = int(input('Enter your number : '))
+    if number == 20 :
+        print('Bang On')
+        break
+    elif number < 20 :
+        print('Too Low')
+    elif number > 20 :
+        print('Too High')
+    total += 1
+
+print('it took you',total,'times to get it')
