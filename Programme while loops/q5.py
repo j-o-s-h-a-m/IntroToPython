1e0739fbe4a79beaffd91d34480fe1c023ef9564
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-07 14:00:55 +0100

    Update q5.py

--- Programme while loops/q5.py
+++ Programme while loops/q5.py
@@ -1,1 +1,10 @@
 
+number = int(input('Input a number : '))
+count = 1
+
+while count != 50 and number:
+    if count <= number :
+        print(count,'squared', count ,'is',count*count)
+    count += 1
+    if count > number:
+        break
