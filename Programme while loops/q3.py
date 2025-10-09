5cfa5b187a6fe1baa7f6305296ca3bea384cc233
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-09 09:38:30 +0100

    Update q3.py

--- Programme while loops/q3.py
+++ Programme while loops/q3.py
@@ -1,3 +1,6 @@
+#Athor = Saksham Joshi
+#Date = 1st of Semptember 2025
+#description = Program to request user name as input and return the same with a greeting
 number = int(input('Input a number : '))
 count = 0
 
