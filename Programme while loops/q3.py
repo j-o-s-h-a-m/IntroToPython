65c4b0501c613ff0b6d32879dd5a8e5a4a0983e6
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-10 08:48:50 +0100

    Update q3.py

--- Programme while loops/q3.py
+++ Programme while loops/q3.py
@@ -1,6 +1,6 @@
 #Athor = Saksham Joshi
 #Date = 1st of Semptember 2025
-#description = Program to request user name as input and return the same with a greeting
+#description = Write a program that takes an integer number and outputs all the even numbers starting from 0 to the number
 number = int(input('Input a number : '))
 count = 0
 
