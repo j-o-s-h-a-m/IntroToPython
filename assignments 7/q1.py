7d0a0cefde2080dc8dbeb45d0fc706f63c31a826
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-10 09:09:56 +0100

    Update q1.py

--- assignments 7/q1.py
+++ assignments 7/q1.py
@@ -1,1 +1,22 @@
+#Athor = Saksham Joshi
+#Date = 1st of Semptember 2025
+#description = Write a program to check if a number is an Armstrong number. An Armstrong 
+#number is a number equal to the sum of each digit raised to the power of the 
+#number of digits..
+wn = 1
 
+number =  input('Enter Your Number: ')
+total = 0
+y = len(number)
+y = int(y)
+
+
+for i in number:
+    i = int(i)
+    i = i**y
+    total += i 
+    
+if total == number :
+    print('This number is an armstrong number')
+else :
+    print('Think about your life decisions')
