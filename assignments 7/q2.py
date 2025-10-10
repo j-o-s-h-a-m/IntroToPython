ca1f3e0057058bf4ddde6557a003ea884edac196
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-10 09:32:21 +0100

    Update q2.py

--- assignments 7/q2.py
+++ assignments 7/q2.py
@@ -1,1 +1,14 @@
 
+n =  2
+total = 0
+
+
+while n != '':
+    number =  input('Enter Your Number: ')
+    number.isdigit()
+    number = int(number)
+    if number == 0 or number == ''  :
+        break
+    total += number
+
+print(total)
