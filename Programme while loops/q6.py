892f69e94fd15ac49e25fabc4810b3b285ed360c
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-10 08:52:31 +0100

    Update q6.py

--- Programme while loops/q6.py
+++ Programme while loops/q6.py
@@ -1,6 +1,6 @@
 #Athor = Saksham Joshi
 #Date = 1st of Semptember 2025
-#description = Program to request user name as input and return the same with a greeting
+#description = Write a program which takes in a numbers from the user. It should continue taking in numbers until the total of all the numbers entered is greater than 50.
 number = int(input('Input a number : '))
 count = 0
 
