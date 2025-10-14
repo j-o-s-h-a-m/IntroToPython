140f80b29c1bb9fa3aadfbd37cb0d6f403378870
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-14 14:05:51 +0100

    Update q4.py

--- assignments 7/q4.py
+++ assignments 7/q4.py
@@ -1,3 +1,7 @@
+#Athor = Saksham Joshi
+#Date = 1st of Semptember 2025
+#description = Write a program to enter numbers until the user enters 0. Then it should print the count of the positive and negative numbers entered. You can assume all input is numeric.
+
 n =  2
 total = 0
 number =  input('Enter Your Number: ')
