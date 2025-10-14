281d75e6df2ca3bc52805ffe0b986e18d0deac85
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-14 14:07:06 +0100

    Update q3.py

--- assignments 7/q3.py
+++ assignments 7/q3.py
@@ -1,6 +1,7 @@
 #Athor = Saksham Joshi
 #Date = 1st of Semptember 2025
-#description = Write a program to enter numbers until the user enters 0. Then it should print the count of the positive and negative numbers entered. You can assume all input is numeric.
+#description = Write a program to convert Binary to Decimal
+
 
 binary = inut('input ninary number')
 total = 0
