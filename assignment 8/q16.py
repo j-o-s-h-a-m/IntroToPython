6bc80c690430d40ee2a4e45cb55a00828c456890
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-14 14:26:17 +0100

    Create q16.py

---
+++ assignment 8/q16.py
@@ -0,0 +1,10 @@
+a =  input('Enter Your Number: ')
+
+for i in a:
+    i = int(i)
+if i == 4 :
+    print('ends with 4')
+elif i == 8:
+    print('ends with 8')
+else:
+    print('ends with niether')
