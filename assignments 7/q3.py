62ee7f1393c646f00d905baec019d4550dc9c591
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-14 14:05:21 +0100

    Update q3.py

--- assignments 7/q3.py
+++ assignments 7/q3.py
@@ -1,3 +1,6 @@
+#Athor = Saksham Joshi
+#Date = 1st of Semptember 2025
+#description = Write a program to enter numbers until the user enters 0. Then it should print the count of the positive and negative numbers entered. You can assume all input is numeric.
 
 binary = inut('input ninary number')
 total = 0
