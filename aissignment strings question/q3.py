6ddde4cda90ab498aa48e982f6604d46e7626427
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-17 09:45:48 +0100

    Update q3.py

--- aissignment strings question/q3.py
+++ aissignment strings question/q3.py
@@ -1,1 +1,12 @@
+word = input('enter your world: ')
+y = len(word)
+n = 1 
 
+if word[0] in 'aeiou':
+  print(word + 'way')
+else :
+  wordnew = ''
+  for i in range (1, len(word)) :
+      wordnew = wordnew + word[i]
+  wordnew = wordnew + word[0]
+  print(wordnew + 'ay')
