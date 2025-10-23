151c7a649f6a517858713cf304207c3f763129ef
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-23 08:58:08 +0100

    Implement character count using for and while loops
    Added a while loop implementation to count characters in a word.

---
+++ strings assignment 2/q2.py
@@ -0,0 +1,19 @@
+
+# A for loop
+word = input('Enter Your word : ')
+count = 0
+for i in word:
+    count += 1
+
+print(count)
+
+#A While loop
+
+word = input('Enter Your word : ')
+count = 0
+length = len(word)
+
+while count < length:
+    count += 1
+
+print(count)
