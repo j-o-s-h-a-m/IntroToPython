fa00b9857a1a33cdb416e1f585fbc9c4e725da9b
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-23 09:36:14 +0100

    Add string replacement for first character
    Implement string transformation to replace first character occurrences.

--- strings assignment 2/q3.py
+++ strings assignment 2/q3.py
@@ -1,3 +1,9 @@
 #Author = Saksham Joshi
 #23/10/2025
 #Create a string from a given string where all occurrences of its first character have been changed to '$', except the first char itself
+
+variable = input('Enter Your word: ')
+v1 = variable[0]
+v2 = variable[1:]
+v3 = v2.replace(v1,'$')
+print(v1 + v3)
