64db9d65e117b586e6a460554c3123df45d754a7
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-24 09:15:47 +0100

    Add program to remove odd index characters from string
    Implement a program to remove characters at odd indices from a string.

---
+++ strings assignment 2/q4.py
@@ -0,0 +1,10 @@
+#Author = Saksham Joshi
+#23/10/2025
+#Write a Python program to remove characters that have odd index values in a given string.
+
+variable = input('Enter Your word: ')
+#v1 = variable[1]
+v2 = variable[0::2]
+#v3 = v2.replace(v1,'')
+#print(v1 + v3)
+print(v2)
