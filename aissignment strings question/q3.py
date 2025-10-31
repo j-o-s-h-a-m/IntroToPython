23d9b111b54f4c751d76fd67996aa0ef0e9455a1
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-31 11:41:56 +0000

    Update q3.py

--- aissignment strings question/q3.py
+++ aissignment strings question/q3.py
@@ -1,6 +1,10 @@
 #Author = Saksham Joshi
 #date = 31/10/2025
-#Ask the user to enter their first name and display the length of their name
+#Ask the user to enter a word and print the word modified as follows:
+#• If the first letter of the word is a vowel, add ‘way’ to the end. So, if the user enters
+#‘apple’ it should output ‘appleway’.
+#• If the first letter of the word is a consonant, move the first letter to the end and
+#add ‘ay’. So, if the user enters ‘pear’, it should output ‘earpay’.
 word = input('enter your world: ')
 y = len(word)
 n = 1 
