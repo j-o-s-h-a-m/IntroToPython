ae9d0ca1c546eea224f410da539e5d968f71e8a8
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-31 11:40:21 +0000

    Update q2.py

--- aissignment strings question/q2.py
+++ aissignment strings question/q2.py
@@ -1,3 +1,6 @@
+#Author = Saksham Joshi
+#date = 31/10/2025
+#Ask the user to enter their first name and display the length of their name
 fname = input('Enter Your first name')
 lname = input('Enter Your last name')
 final_name = fname+' '+lname
