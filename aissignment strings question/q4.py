038e1d7f8360c19fd92a16675b6fd06e1eaf1050
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-31 11:48:21 +0000

    Update q4.py

--- aissignment strings question/q4.py
+++ aissignment strings question/q4.py
@@ -1,3 +1,12 @@
 #Author = Saksham Joshi
 #date = 31/10/2025
 #Write a program to count the number of times a character occurs in the given string.The string and the character should be input by the user.
+
+word = input(' enter a word of your choice : ')
+letter = input('enter a letter of your choice : ')
+count = 0
+
+for i in word:
+  if i == letter:
+    count += 1
+print('The letter',letter,'occurs the following times : ',count)
