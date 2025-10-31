6cd666d26ef4a980e9b70736b58aec679207bf7c
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-31 12:49:08 +0000

    Update q15.py

--- assignment 8/q15.py
+++ assignment 8/q15.py
@@ -1,3 +1,6 @@
+#Author = Saksham Joshi
+#date = 31/10/2025
+#Write a short program to find largest number of a list of numbers entered through keyboard.
 number = input('enter a list of numbers in the form a , b , c ,d:')
 largest = number[0]
 
