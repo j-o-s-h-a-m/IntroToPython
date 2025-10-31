2b1c451f4836180e40e60a318f3835e1620868b9
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-31 11:42:15 +0000

    Update q4.py

--- aissignment strings question/q4.py
+++ aissignment strings question/q4.py
@@ -1,3 +1,3 @@
 #Author = Saksham Joshi
 #date = 31/10/2025
-#Ask the user to enter their first name and display the length of their name
+#Write a program to count the number of times a character occurs in the given string.The string and the character should be input by the user.
