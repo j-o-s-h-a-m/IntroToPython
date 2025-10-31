813f1310d8ef53c6eee2b1eefdcbf31d5ba3e3ce
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-31 11:50:38 +0000

    Update q13.py

--- assignment 8/q13.py
+++ assignment 8/q13.py
@@ -1,3 +1,7 @@
+#Author = Saksham Joshi
+#date = 31/10/2025
+#Write a program to take an integer a as an input and check whether it ends with 4 or 8. If it ends with 4,print "ends with 4", if it ends with 8, print "ends with 8", otherwise print "ends with neither".
+
 a =  input('Enter Your Number: ')
 
 for i in a:
