0092325df1bc6654094635277e4ff2ae9c4566e5
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-31 11:51:28 +0000

    Update q14.py

--- assignment 8/q14.py
+++ assignment 8/q14.py
@@ -1,3 +1,8 @@
+#Author = Saksham Joshi
+#date = 31/10/2025
+#Write a program to take N (N > 20) as an input from the user. Print numbers from 11 to N. When the
+#number is a multiple of 3, print "Tipsy", when it is a multiple of 7, print "Topsy". When it is a multiple
+#of both, print "TipsyTopsy".
 x = int(input('enter a number less than 20'))
 
 if x%3 == 0:
