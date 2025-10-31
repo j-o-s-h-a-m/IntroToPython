5879e705a302bae37523c04af5ffe79d8d17294f
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-31 11:41:29 +0000

    Modify input to include surname and display full name
    Updated the prompt to ask for both first and last names, and modified the output to show the full name and its length.

--- aissignment strings question/q2.py
+++ aissignment strings question/q2.py
@@ -1,6 +1,6 @@
 #Author = Saksham Joshi
 #date = 31/10/2025
-#Ask the user to enter their first name and display the length of their name
+#Ask the user to enter their first name and then their surname. Join them togetherwith a space between and then display their full name and the length of their full name
 fname = input('Enter Your first name')
 lname = input('Enter Your last name')
 final_name = fname+' '+lname
