0637faa97ea48226ce7402179fe38a6f3dea309f
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-31 13:32:41 +0000

    Update q20.py

--- assignment 8/q20.py
+++ assignment 8/q20.py
@@ -1,1 +1,18 @@
 
+
+
+#a
+
+# didnt get this part  
+    
+    
+
+#b
+count = 0
+n = input('enter your number : ')
+n = int(n)
+for i in range(1,n):
+    y = i**2
+    count = 0+y
+print(count)
+    
