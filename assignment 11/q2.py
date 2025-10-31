ca3f02ddf95e94bf437b62e2b0fdc1ce4695327c
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-31 15:10:34 +0000

    Implement palindrome check in q2.py
    Added functionality to check if a string is a palindrome.

--- assignment 11/q2.py
+++ assignment 11/q2.py
@@ -1,2 +1,12 @@
+#Author = Saksham joshi
+#Date = 31/10/2025
+#Write a program to check if a string is a palindrome. A palindrome is a word or string
+#that reads the same backwards and forwards. For example: racecar; rotator
+
 word = input('enter your word : ')
-word2 = 
+word2 = word[::-1]
+
+if word2 == word :
+  print('its a palindrome')
+else:
+  print(word2)
