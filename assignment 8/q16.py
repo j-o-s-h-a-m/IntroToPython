1910c57072373ca4a998a547e4ccb6031d79416c
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-10-31 13:01:00 +0000

    Update q16.py

--- assignment 8/q16.py
+++ assignment 8/q16.py
@@ -1,3 +1,18 @@
 #Author = Saksham Joshi
 #date = 31/10/2025
 #Write a program to input N numbers and then print the second largest number.
+
+number = input('enter a list of numbers in the form a , b , c ,d:')
+largest = number[0]
+
+
+for i in number:
+    if i > largest:
+        largest = i
+    
+for val in number:
+    if val < largest:
+            largest2 = val
+
+        
+print(largest2)
