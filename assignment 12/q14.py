77822eb8dccb6d7268c2195ca1b93206b8c5412d
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-11-06 09:09:43 +0000

    Update q14.py

--- assignment 12/q14.py
+++ assignment 12/q14.py
@@ -2,3 +2,10 @@
 #Date = 3/11/2008
 #write a program to count  a number of vowels 
 
+x = input('Enter a string : ')
+vowels = 0
+for i in x :
+    if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u':
+        vowels += 1
+    
+print('There are',vowels,'vowels in this string')
