9430b90500d281f8115d3d0a8e18d57645f5779c
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-11-17 13:00:04 +0000

    Create binary adder.py

---
+++ binary adder/binary adder.py
@@ -0,0 +1,7 @@
+binary_number = input('Enter Your first binary Number :  ')
+binary_number2 = input('Enter Your second binary Number :  ')
+
+if len(binary_number) == (binary_number):
+    for i in
+
+
