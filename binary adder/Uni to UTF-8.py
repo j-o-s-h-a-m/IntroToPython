9ac6d94cedd671fceeaaf0ef16e8d45d7f7c0ca5
JOSHAM <22SJoshi.acc@lwetb.ie>
2025-11-27 09:45:05 +0000

    Update Uni to UTF-8.py

--- binary adder/Uni to UTF-8.py
+++ binary adder/Uni to UTF-8.py
@@ -1,2 +1,15 @@
-x = input('Enter  An UNICODE : ')
+x = input('Enter  An UNICODE : ').upper() 
+
 x = x[2:len(x)+1:]
+print(x)
+
+for i in x :
+    x.isdigit():
+        n = i
+    while n =! 
+        
+        
+        
+    
+
+        
