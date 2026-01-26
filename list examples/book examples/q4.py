deef589bed0756c44a092ce38d8855fac55f3272
JOSHAM <22SJoshi.acc@lwetb.ie>
2026-01-26 12:49:49 +0000

    Fix input handling and update values in list
    Convert input to list and fix comparison logic.

--- list examples/book examples/q4.py
+++ list examples/book examples/q4.py
@@ -1,7 +1,9 @@
 lst1 = eval(input('Enter a number between 1 and 12 : '))
-count = 0 
+lst1 = list(lst1)
+count = 0
+print(lst1)
 for i in lst1 :
-  if i >= 10 :
-    lst[count] = 10
+  if int(i) > 10 :
+    lst1[count] = 10
   count += 1 
 print(lst1)
