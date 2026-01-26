4bd85725cf1b6c30e947f2467965bc21da6aeeb6
JOSHAM <22SJoshi.acc@lwetb.ie>
2026-01-26 12:59:42 +0000

    Update q5.py

--- list examples/book examples/q5.py
+++ list examples/book examples/q5.py
@@ -1,6 +1,13 @@
 lst1 = []
+lst1 = list(lst1)
+print(lst1)
 n = 0 
 while n == 0:
-  lst = eval(input('Enter a list of numbers : '))
-  lst1 = lst1 + lst[1,len(lst)]
+    lst = eval(input('Enter a list of numbers : '))
+    if lst == 
+    lst = list(lst)
+    print(lst)
+    lst1 = list(lst1) + list(lst[1:len(lst)])
+    
+  
 print(lst1)
