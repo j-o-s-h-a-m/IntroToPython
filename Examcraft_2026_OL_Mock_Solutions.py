0ca1bfd27a0a519be81c877b4ba8901efe6f59da
JOSHAM <22SJoshi.acc@lwetb.ie>
2026-04-25 01:03:09 +0100

    Update Examcraft_2026_OL_Mock_Solutions.py

--- Examcraft_2026_OL_Mock_Solutions.py
+++ Examcraft_2026_OL_Mock_Solutions.py
@@ -1,26 +1,15 @@
-# ============================================================
-# Examcraft 2026 Pre-Leaving Certificate Mock - Ordinary Level
-# Computer Science Section C - Programming Solutions
-# ============================================================
 
 
-# ------------------------------------------------------------
-# Question 16(a) - Step Tracker App
-# ------------------------------------------------------------
 
 print("Welcome to the Step Tracker App!")
 
-# Part (iii) - Ask user for their name
 name = input("Please enter your name: ")
 
-# Part (i) - Comment on the input line
 steps_today = int(input("Enter the number of steps you walked today: ")) #this is where the user enters the number of steps
 
-# Part (iv) - Handle negative steps
 if steps_today < 0:
     print("The number of steps cannot be negative")
 
-# Part (v) - Categorise the user's day
 elif steps_today < 5000:
     print(f"Try to be more active today {name}")
 elif steps_today < 10000:
@@ -29,9 +18,6 @@ else:
     print(f"Well done {name} you reached your goal!")
 
 
-# ------------------------------------------------------------
-# Question 16(b) - Weekly Step Tracker
-# ------------------------------------------------------------
 
 print("Welcome to my weekly step tracker!")
 
